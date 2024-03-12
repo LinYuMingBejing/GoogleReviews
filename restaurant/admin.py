b582ee7827ba31c7b5cbdaefdc06929259d6b8f2
jack-y-lin <jack_y_lin@trendmicro.com>
2024-03-12 22:17:19 +0800

    Delete unused file

--- restaurant/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from restaurant.models import Restaurant, Review
-
-# Register your models here.
-
-admin.site.register(Restaurant)
-admin.site.register(Review)
--- user/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-import django.utils.timezone as timezone
-
-# Create your models here.
